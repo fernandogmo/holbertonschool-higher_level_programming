81347c52f993e2f3f3cbca3a7446168fdf4f0edf
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-05 06:46:01 +0000

    Fixed negative digit case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = int(str(number)[-1])
-cond = "greater than 5" if n > 5 else \
-       "less than 6 and not 0" if n > 0 else \
-       "zero"
+n = number % -10 if number < 0 else number % 10
+cond = "zero" if n == 0 else \
+       "greater than 5" if n > 5 else \
+       "less than 6 and not 0"
 print("Last digit of {:d} is {:d} and is {}".format(number, n, cond))
