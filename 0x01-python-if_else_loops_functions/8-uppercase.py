6bd082fee8a39d03059053103eab0efb4075a410
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-05 06:54:37 +0000

    Added format in print for opinionated auto-checker

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(s):
     for c in s:
         delta = ord('A') - ord('a') if ord('a') <= ord(c) <= ord('z')\
                                     else 0
-        print(chr(ord(c) + delta), end="")
+        print("{}".format(chr(ord(c) + delta)), end="")
     print()
