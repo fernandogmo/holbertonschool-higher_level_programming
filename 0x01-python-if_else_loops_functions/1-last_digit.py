a5168aceb64d73c5c2440f87884fa0d5dc9fc9ea
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-05 06:49:02 +0000

    Fixed case of last digit zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 n = number % -10 if number < 0 else number % 10
-cond = "zero" if n == 0 else \
+cond = "0" if n == 0 else \
        "greater than 5" if n > 5 else \
        "less than 6 and not 0"
 print("Last digit of {:d} is {:d} and is {}".format(number, n, cond))
