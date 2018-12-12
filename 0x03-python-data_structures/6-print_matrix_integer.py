99879d282246607c7c340c80e38adf4e9783905b
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-12 05:44:24 +0000

    Updated str.format() template string

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,16 @@
 # str.format() when not necessary.
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        print(("{} {} {}" if len(x) else "").format(*x))
+        print(("{:d} {:d} {:d}" if len(x) else "").format(*x))
+
+
+matrix = [
+            [1, 2, 3],
+                [4, 5, 6],
+                    [7, 8, 9]
+                    ]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
+
