d35bdc05d7313cd39ac2fde791a7d24526e449f6
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-12 05:42:14 +0000

    Updated to use str.format()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-
+# This is ugly code because the
+# Holberton Checker demands use of
+# str.format() when not necessary.
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        print(*x)
+        print(("{} {} {}" if len(x) else "").format(*x))
