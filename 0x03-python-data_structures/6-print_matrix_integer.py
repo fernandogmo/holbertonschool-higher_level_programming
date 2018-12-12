77913ebcc69bc55b2e123d175dbe227ab18e009f
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-12 05:45:55 +0000

    Fixed PEP8 issues

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 
+
 # This is ugly code because the
 # Holberton Checker demands use of
 # str.format() when not necessary.
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         print(("{:d} {:d} {:d}" if len(x) else "").format(*x))
-
-
-matrix = [
-            [1, 2, 3],
-                [4, 5, 6],
-                    [7, 8, 9]
-                    ]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
-
