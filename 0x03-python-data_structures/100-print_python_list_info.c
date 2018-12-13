031cfdaed91647bf72171d49aef44a46931b8992
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-13 07:02:30 +0000

    Added file for advanced task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include "Python.h"
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t i = 0, size = Py_SIZE(p);
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+        for (; i < size; i++)
+		printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
