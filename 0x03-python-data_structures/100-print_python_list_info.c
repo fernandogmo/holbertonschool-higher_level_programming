e21ff436ecee0cba384518eb2793c3f7f56b2a19
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-13 07:30:46 +0000

    Formatting fixes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,10 +2,11 @@
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i = 0, size = Py_SIZE(p);
+	Py_ssize_t size = Py_SIZE(p);
 
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-        for (; i < size; i++)
+
+	for (Py_ssize_t i = 0; i < size; i++)
 		printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
