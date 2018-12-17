8c69f89c4f8ef803ca3e255106a5f1ff699455bd
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-17 03:19:23 +0000

    Added typecheck for string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev = ''
-    if roman_string:
+    if roman_string and type(roman_string) == str:
         for r in roman_string:
             if r == 'I':
                 total += 1
