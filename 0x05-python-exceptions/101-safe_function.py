dd9787525795b10906a7ea88eccd67b68d612a0a
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-01-02 23:49:26 -0800

    Added file for Advanced Task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as err:
+        from sys import stderr
+        print("Exception: {}".format(err), file=stderr)
+        return None
+
+
+if __name__ == '__main__':
+    def my_div(a, b):
+        return a / b
+
+    result = safe_function(my_div, 10, 2)
+    print("result of my_div: {}".format(result))
+
+    result = safe_function(my_div, 10, 0)
+    print("result of my_div: {}".format(result))
+
+    def print_list(my_list, len):
+        i = 0
+        while i < len:
+            print(my_list[i])
+            i += 1
+        return len
+
+    result = safe_function(print_list, [1, 2, 3, 4], 10)
+    print("result of print_list: {}".format(result))
