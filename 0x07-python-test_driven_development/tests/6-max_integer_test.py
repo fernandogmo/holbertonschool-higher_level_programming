3a79c822eb3eee81b0a28f1f608f86d81d70d91b
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-01-13 20:25:29 -0800

    Added more tests for 6-max_integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,11 +13,23 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_single(self):
+        self.assertEqual(max_integer([3]), 3)
+
     def test_ascending(self):
-        self.assertEqual(max_integers([1, 2, 3]), 3)
+        self.assertEqual(max_integer([1, 2, 3]), 3)
 
     def test_descending(self):
-        self.assertEqual(max_integers([3, 2, 1]), 3)
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_middle(self):
+        self.assertEqual(max_integer([2, 3, 1]), 3)
+
+    def test_single_negative(self):
+        self.assertEqual(max_integer([2, 3, -1]), 3)
+
+    def test_all_negatives(self):
+        self.assertEqual(max_integer([-2, -3, -1]), -1)
 
     def tearDown(self):
         pass
