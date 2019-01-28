63579b29ebbf15989ba07ff0f3a736955972955d
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-01-27 23:53:06 -0800

    Added rectangle tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, v):
         """ `__width` setter """
+        if type(v) is not int:
+            raise TypeError("width must be an integer")
+        if v <= 0:
+            raise ValueError("width must be > 0")
         self.__width = v
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, v):
         """ `__height` setter """
+        if type(v) is not int:
+            raise TypeError("width must be an integer")
+        if v <= 0:
+            raise ValueError("width must be > 0")
         self.__height = v
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, v):
         """ `__x` setter """
+        if type(v) is not int:
+            raise TypeError("x must be an integer")
+        if v < 0:
+            raise ValueError("x must be >= 0")
         self.__x = v
 
     @property
@@ -60,7 +72,64 @@ class Rectangle(Base):
     @y.setter
     def y(self, v):
         """ `__width` setter """
+        if type(v) is not int:
+            raise TypeError("y must be an integer")
+        if v < 0:
+            raise ValueError("y must be >= 0")
         self.__y = v
+
+    def area(self):
+        """ Calculates area of rectangle """
+        return self.width * self.height
+
+    def display(self):
+        """ Displays rectangle using `#` character """
+
+        for row in range(self.y):
+            print()
+        for row in range(self.height):
+            print('{}{}'.format(' ' * self.x, '#' * self.width))
+
+    def __str__(self):
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(
+                            self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """updates multiple attributes"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.width = a
+                elif i == 2:
+                    self.height = a
+                elif i == 3:
+                    self.x = a
+                elif i == 4:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """dictionary representation of a Rectangle"""
+        d = {}
+        d["id"] = self.id
+        d["width"] = self.width
+        d["height"] = self.height
+        d["x"] = self.x
+        d["y"] = self.y
+        return d
+
 
 if __name__ == '__main__':
     r1 = Rectangle(10, 2)
@@ -71,3 +140,25 @@ if __name__ == '__main__':
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
+
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
