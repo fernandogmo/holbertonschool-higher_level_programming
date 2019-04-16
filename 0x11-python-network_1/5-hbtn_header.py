737873891ccb2f49049b1609bc62de832b7af3ba
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-15 23:42:24 -0700

    Added WIP files for task 5 thru 7

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,6 @@ the value of the variable `X-Request-Id` in the response header.
 import requests
 import sys
 
-
 if __name__ == '__main__':
     response = requests.get(sys.argv[1])
     print(response.headers.get('X-Request-Id'))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,6 @@ and finally displays the body of the response.
 import requests
 import sys
 
-
 if __name__ == '__main__':
     response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(response.text)
