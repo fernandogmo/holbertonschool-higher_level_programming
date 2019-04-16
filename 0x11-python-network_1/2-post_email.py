47a686050b7954e1e1e440accfe189f8c0a6568c
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-04-15 17:34:36 -0700

    Added WIP files for tasks 1 and 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8).
+'''
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    data = urllib.parse({'email': sys.argv[2]}).encode()
+    with urllib.request.urlopen(Request(sys.argv[1], data)) as response:
+	print(response.read().decode('utf_8'))
