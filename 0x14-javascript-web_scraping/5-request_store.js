af1a65d1ca84f2082079cd6887f4d487e895ccd2
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-15 18:22:41 -0700

    Used pipe

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-require('request').get(process.argv[2], (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    require('fs').writeFile(process.argv[3], body, 'utf8', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+require('request').get(process.argv[2])
+  .pipe(require('fs').createWriteStream(process.argv[3]));
